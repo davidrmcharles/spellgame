e7d3707e1844c5e2447e151fa30d30da4cbedbce
davidrmcharles <31576489+davidrmcharles@users.noreply.github.com>
2020-10-31 09:39:22 -0700

    Add a TestCorrectEntryReturn

--- setest/setest_spellgame.py
+++ setest/setest_spellgame.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import os
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 _setest_folder = os.path.dirname(os.path.abspath(__file__))
@@ -43,6 +45,50 @@ class TestInit(unittest.TestCase):
     def _test_inputtext_is_focused(self):
         elem = self.driver.find_element_by_id('user-entry-text')
         self.assertEqual(self.driver.switch_to.active_element, elem)
+
+
+class TestCorrectEntryReturn(unittest.TestCase):
+    '''
+    Test the response to a correct entry with the RETURN key.
+    '''
+
+    def test_chrome(self):
+        self.driver = webdriver.Chrome()
+        self._test()
+
+    def test_firefox(self):
+        self.driver = webdriver.Firefox()
+        self._test()
+
+    def _test(self):
+        try:
+            self.driver.get(f'file:///{_project_folder}/src/spellgame.html?istest=true')
+            elem = self.driver.find_element_by_id('user-entry-text')
+            elem.send_keys('some')
+            elem.send_keys(Keys.RETURN)
+            self._test_progress_has_text()
+            self._test_feedback_has_text()
+            self._test_inputtext_is_focused()
+            time.sleep(2)
+            self._test_feedback_has_text_2()
+        finally:
+            self.driver.close()
+
+    def _test_progress_has_text(self):
+        elem = self.driver.find_element_by_id('progress')
+        self.assertEqual('Progress: 1 / 19', elem.text)
+
+    def _test_feedback_has_text(self):
+        elem = self.driver.find_element_by_id('feedback')
+        self.assertIn('correct', elem.text)
+
+    def _test_inputtext_is_focused(self):
+        elem = self.driver.find_element_by_id('user-entry-text')
+        self.assertEqual(self.driver.switch_to.active_element, elem)
+
+    def _test_feedback_has_text_2(self):
+        elem = self.driver.find_element_by_id('feedback')
+        self.assertIn('here', elem.text)
 
 
 if __name__ == '__main__':
