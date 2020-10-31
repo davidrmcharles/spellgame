f54439d7ef7fcdd4ae21ee29e3f9876b588c247c
davidrmcharles <31576489+davidrmcharles@users.noreply.github.com>
2020-10-31 09:51:17 -0700

    Move the GET and driver.close() into a base class

--- setest/setest_spellgame.py
+++ setest/setest_spellgame.py
@@ -12,27 +12,35 @@ _setest_folder = os.path.dirname(os.path.abspath(__file__))
 _project_folder = os.path.dirname(_setest_folder)
 
 
-class TestInit(unittest.TestCase):
+class _TestBase(unittest.TestCase):
+
+    def _test_with_driver(self):
+        try:
+            self.driver.get(
+                f'file:///{_project_folder}/src/spellgame.html?istest=true'
+            )
+            self._test_with_page()
+        finally:
+            self.driver.close()
+
+
+class TestInit(_TestBase):
     '''
     Test the initial state of the page.
     '''
 
     def test_chrome(self):
         self.driver = webdriver.Chrome()
-        self._test()
+        self._test_with_driver()
 
     def test_firefox(self):
         self.driver = webdriver.Firefox()
-        self._test()
+        self._test_with_driver()
 
-    def _test(self):
-        try:
-            self.driver.get(f'file:///{_project_folder}/src/spellgame.html?istest=true')
-            self._test_progress_has_text()
-            self._test_feedback_has_text()
-            self._test_inputtext_is_focused()
-        finally:
-            self.driver.close()
+    def _test_with_page(self):
+        self._test_progress_has_text()
+        self._test_feedback_has_text()
+        self._test_inputtext_is_focused()
 
     def _test_progress_has_text(self):
         elem = self.driver.find_element_by_id('progress')
@@ -47,32 +55,28 @@ class TestInit(unittest.TestCase):
         self.assertEqual(self.driver.switch_to.active_element, elem)
 
 
-class TestCorrectEntryReturn(unittest.TestCase):
+class TestCorrectEntryReturn(_TestBase):
     '''
     Test the response to a correct entry with the RETURN key.
     '''
 
     def test_chrome(self):
         self.driver = webdriver.Chrome()
-        self._test()
+        self._test_with_driver()
 
     def test_firefox(self):
         self.driver = webdriver.Firefox()
-        self._test()
+        self._test_with_driver()
 
-    def _test(self):
-        try:
-            self.driver.get(f'file:///{_project_folder}/src/spellgame.html?istest=true')
-            elem = self.driver.find_element_by_id('user-entry-text')
-            elem.send_keys('some')
-            elem.send_keys(Keys.RETURN)
-            self._test_progress_has_text()
-            self._test_feedback_has_text()
-            self._test_inputtext_is_focused()
-            time.sleep(2)
-            self._test_feedback_has_text_2()
-        finally:
-            self.driver.close()
+    def _test_with_page(self):
+        elem = self.driver.find_element_by_id('user-entry-text')
+        elem.send_keys('some')
+        elem.send_keys(Keys.RETURN)
+        self._test_progress_has_text()
+        self._test_feedback_has_text()
+        self._test_inputtext_is_focused()
+        time.sleep(2)
+        self._test_feedback_has_text_2()
 
     def _test_progress_has_text(self):
         elem = self.driver.find_element_by_id('progress')
